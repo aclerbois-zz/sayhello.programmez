77846060e99451471331127bc17918fd86969441
Adrien Clerbois <adrien@aclerbois.be>
2018-03-28 21:03:42 +0200

    Add CLI tool project

---
+++ aclerbois.sayhello.programmez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aclerbois.sayhello.programmez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut les lecteurs du magazine Programmez!");
+        }
+    }
+}
